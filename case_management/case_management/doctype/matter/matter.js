199b2b00450a802863903d8f6ede65a4d6faff03
Bobby <bobzz.zone@gmail.com>
2016-10-30 17:33:09 +0000

    as

--- case_management/case_management/doctype/matter/matter.js
+++ case_management/case_management/doctype/matter/matter.js
@@ -4,13 +4,20 @@
 frappe.ui.form.on('Matter', {
 	refresh: function(frm) {
 		if (frm.doc.docstatus==1){
-			frm.add_custom_button(__('Make Invoice'), this.make_invoice);
+			frm.add_custom_button(__('Make Invoice'),function() {
+//                alert(1);
+                frappe.model.open_mapped_doc({
+                        method: "case_management.case_management.doctype.matter.matter.make_invoice",
+                        frm: cur_frm
+                });
+});
 		}
 	},
 });
-cur_frm.cscript.make_invoice= function() {
-		frappe.model.open_mapped_doc({
-			method: "case_management.case_management.doctype.matter.matter.make_invoice",
-			frm: cur_frm
-		});
-	}
+//cur_frm.cscript.make_invoice= function() {
+//		alert(1);
+//		frappe.model.open_mapped_doc({
+//			method: "case_management.case_management.doctype.matter.matter.make_invoice",
+//			frm: cur_frm
+//		});
+//	}
