7c9c8f398dc141ba021e86dd66d247fa3a80fdd5
Bobby <bobzz.zone@gmail.com>
2016-10-30 15:36:47 +0000

    ta

--- case_management/config/case_management.py
+++ case_management/config/case_management.py
@@ -2,22 +2,31 @@ from __future__ import unicode_literals
 from frappe import _
 
 def get_data():
+	return []
+def get_x():
 	return [
 		{
-			"label": _("Documents"),
+			"label": _("Case Documents"),
 			"items": [
 				{
 					"type": "doctype",
 					"name": "Matter",
 					"description": _("Matter Documents")
-				},
+				}
+			]
+		},
+		{
+			"label":_("Setup"),
+			"items": [
                                 {
                                         "type": "doctype",
-                                        "name": "Practice Area"
+                                        "name": "Practice Area",
+                                        "description": _("Practice Area")
                                 },
                                 {
                                         "type": "doctype",
-                                        "name": "Matter Custom Check List"
+                                        "name": "Matter Custom Check List",
+                                        "description": _("Matter Custom Check List")
                                 },
 			]
 		}
