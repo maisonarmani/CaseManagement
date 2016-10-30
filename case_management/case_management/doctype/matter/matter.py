1cccef9703d55b0dba9187a9e9fd06fcbf1a6e45
bobzz-zone <bobzz.zone@gmail.com>
2016-10-30 23:02:43 +0700

    tes

--- case_management/case_management/doctype/matter/matter.py
+++ case_management/case_management/doctype/matter/matter.py
@@ -8,3 +8,12 @@ from frappe.model.document import Document
 
 class Matter(Document):
 	pass
+	def get_custom_field(self):
+		if self.custom_field :
+			data = frappe.db.sql("""select title from `tabMatter Custom Check List Item` where parent="{}" """.format(self.custom_field),as_list=1)
+			self.check_list=[]
+			for row in data:
+				dt = self.append('check_list', {})
+				dt.title=row[0]
+		else:
+			frappe.throw("""Please select a Custom Field Preset. """)
