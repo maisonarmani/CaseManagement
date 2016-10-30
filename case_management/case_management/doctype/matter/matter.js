13b35f0c527d531cbe229636d4a72be5ffbc9b75
bobzz-zone <bobzz.zone@gmail.com>
2016-10-31 00:14:04 +0700

    aa

--- case_management/case_management/doctype/matter/matter.js
+++ case_management/case_management/doctype/matter/matter.js
@@ -6,7 +6,7 @@ frappe.ui.form.on('Matter', {
 		if (frm.doc.docstatus==1){
 			frm.add_custom_button(__('Make Invoice'), this.make_invoice);
 		}
-	},make_delivery_note: function() {
+	},make_invoice: function() {
 		frappe.model.open_mapped_doc({
 			method: "case_management.case_management.doctype.matter.matter.make_invoice",
 			frm: cur_frm
